50e613217088cd3a5d44d2385335b4f39f2c2e23
Trung Lee <lvbt1002@gmail.com>
2025-07-10 22:36:36 +0700

    Search and view Homestay

---
+++ DTOs/HomestayDtos/HomestayListDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs
+{
+    public class HomestayListDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StreetAddress { get; set; }
+        public string WardName { get; set; }  
+        public bool Status { get; set; }
+        public string ThumbnailUrl { get; set; } // Nếu có ảnh đại diện
+    }
+}
+
